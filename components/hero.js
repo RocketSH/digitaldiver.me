4989d679e280d77f72fa76c2bb1832be78206b17
digital-diver <digital-diver@protonmail.com>
2021-05-22 12:50:17 +0800

    Initial commit

---
+++ components/hero.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section className="hero is-large">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1 className="subtitle">Hi there, I am</h1>
+          <h1 className="title">Shannon Hu</h1>
+          <h1 className="subtitle profession">
+            A Frontend Developer&amp; Freediving Lover
+          </h1>
+          <div />
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Hero;
